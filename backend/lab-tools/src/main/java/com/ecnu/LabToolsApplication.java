0a0b35b1f7375e7723cb04fca4dc6c79a4939c0c
markkobs <1624912549@qq.com>
2021-05-24 10:51:10 +0800

    springboot init

---
+++ backend/lab-tools/src/main/java/com/ecnu/LabToolsApplication.java
@@ -0,0 +1,13 @@
+package com.ecnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabToolsApplication.class, args);
+	}
+
+}
---
+++ backend/lab-tools/src/test/java/com/ecnu/LabToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
